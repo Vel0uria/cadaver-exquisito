b0612f9d0bb694f0e832be213bf0acd8c3118290
VelOuria <sofleos@gmail.com>
2021-05-12 23:18:36 -0500

    added columns

--- cadaver_1/rectClass.js
+++ cadaver_1/rectClass.js
@@ -1,19 +1,17 @@
 class Rectangle {
-  constructor(y) {
-    this.x = width / 2;
+  constructor(x, y) {
+    this.x = x;
     this.y = y;
     this.rectWidth = rectWidth;
     this.rectHeight = rectHeight;
-    this.ySpeed = 4;
+    this.ySpeed = 3;
+    this.modeAr = [LIGHTEST, DARKEST, DIFFERENCE, BLEND];
   }
 
   display(pic) {
-    // tint(g, r, b);
-    //  blendMode(DIFFERENCE);
-
     image(
       pic,
-      width / 2,
+      this.x,
       this.y,
       this.rectWidth,
       this.rectHeight,
@@ -26,14 +24,39 @@ class Rectangle {
 
   move() {
     this.y -= this.ySpeed;
-    if (this.y < -1000) {
-      this.y = height;
+    if (this.y < -140) {
+      this.y = height + this.rectHeight;
     }
   }
 
   changeColor() {
-    r = map(this.y, height - this.rectHeight, -1000, noise(colorR) * 255, 255);
-    g = map(this.y, height - this.rectHeight, -1000, noise(colorG) * 255, 255);
-    b = map(this.y, height - this.rectHeight, -1000, noise(colorB) * 255, 255);
+    colorR = colorR + 0.0001;
+    colorG = colorG + 0.0002;
+    colorB = colorB + 0.0003;
+    r = map(this.y, height - this.rectHeight, -140, noise(colorR) * 255, 255);
+    g = map(this.y, height - this.rectHeight, -140, noise(colorG) * 255, 255);
+    b = map(this.y, height - this.rectHeight, -140, noise(colorB) * 255, 255);
+  }
+
+  randomMode() {
+    let when = map(
+      this.y,
+      height - this.rectHeight,
+      -1000,
+      this.modeAr.length,
+      0,
+      3
+    );
+    if (this.y <= when && this.y > -1) {
+      blendMode(random(this.modeAr));
+      dx = random(200, 500);
+    }
+
+    //  for (let i = 0; i < when.length; i++) {
+    //    if(this.y == i){
+    // blendMode();
+    //  }
+    //
+    // }
   }
 }
--- cadaver_1/sketch.js
+++ cadaver_1/sketch.js
@@ -1,15 +1,19 @@
 const rectHeight = 247;
 const rectWidth = 140;
 let images = [];
+let images_1 = [];
+let images_2 = [];
 const rectangles = [];
+const rectangles_1 = [];
+const rectangles_2 = [];
 let set;
 let colorR = 0.0;
 let colorG = 0.0;
 let colorB = 0.0;
-let r = 150;
-let g = 150;
-let b = 150;
-
+let r = 50;
+let g = 50;
+let b = 50;
+let tristeza;
 function preload() {
   exquisito = loadImage("assets/exquisito.jpg");
   pollo = loadImage("assets/pollo.jpg");
@@ -21,50 +25,69 @@ function preload() {
   gratis = loadImage("assets/gratis.jpg");
   peru = loadImage("assets/peru.jpg");
   abismo = loadImage("assets/abismo.jpg");
+  tristeza = loadImage("assets/tristeza.jpg");
+  aware = loadImage("assets/aware.jpeg");
+  app = loadImage("assets/app.png");
+  descargar = loadImage("assets/descargar.png");
+  codigo = loadImage("assets/codigo.png");
+  empresas = loadImage("assets/empresas.jpg");
+  internet = loadImage("assets/internet.png");
+  desventajas = loadImage("assets/desventajas.jpg");
+  checklist = loadImage("assets/checklist.jpg");
+  complete = loadImage("assets/complete.jpg");
+  programas = loadImage("assets/programas.png");
 }
 
 function setup() {
   createCanvas(1920, 1080);
 
   y = height;
-  dx = random(200, 500);
+  xPos = width / 4;
+  xPos1 = 960;
+  xPos2 = 1420;
+  dx = 390;
   dy = random(150, 300);
-  images = [
-    exquisito,
-    pollo,
-    palabras,
-    otras,
-    vacuna,
-    una,
-    modo,
-    gratis,
-    peru,
-    abismo,
+  images = [exquisito, pollo, palabras, otras, vacuna, una, modo];
+
+  images_1 = [gratis, peru, abismo, tristeza, aware, app, descargar];
+
+  images_2 = [
+    codigo,
+    empresas,
+    internet,
+    desventajas,
+    checklist,
+    complete,
+    programas,
   ];
-  set = new Set(images);
+
   for (let i = 0; i < images.length; i++) {
     y = y + rectHeight;
-    rectangles.push(new Rectangle(y));
+
+    rectangles.push(new Rectangle(xPos, y));
+    rectangles_1.push(new Rectangle(xPos1, y));
+    rectangles_2.push(new Rectangle(xPos2, y));
   }
   imageMode(CENTER);
-  blendMode(DIFFERENCE);
-  //
 }
 
 function draw() {
   background(r, g, b);
 
-  // if (r < 50) {
-  //   blendMode(LIGHTEST);
-  // } else {
-  //   blendMode(BURN);
-  // }
-  colorR = colorR + 0.001;
-  colorG = colorG + 0.002;
-  colorB = colorB + 0.003;
   for (let i = 0; i < rectangles.length; i++) {
     rectangles[i].display(images[i]);
     rectangles[i].move();
     rectangles[i].changeColor();
+    rectangles[i].randomMode();
+  }
+  for (let i = 0; i < rectangles_1.length; i++) {
+    rectangles_1[i].display(images_1[i]);
+    rectangles_1[i].move();
+    // rectangles_1[i].changeColor();
+  }
+  for (let i = 0; i < rectangles_2.length; i++) {
+    rectangles_2[i].display(images_2[i]);
+    rectangles_2[i].move();
+    // rectangles_2[i].changeColor();
   }
 }
